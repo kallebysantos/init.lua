3b9ee362a21bbff435119a8b07e7d7dbe8b5650a
kalleby santos <kalleby_santos@hotmail.com>
2023-05-18 10:03:01 +0100

    📦 lualine, displaying current time

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,5 +1,11 @@
---vim.cmd([[autocmd VimEnter * AirlineTheme minimalist]])
+local function current_time()
+    return os.date('%H:%M')
+end
 
 require('lualine').setup({
-    options = { theme = 'iceberg_dark' }
+    options = { theme = 'iceberg_dark' },
+    sections = {
+        lualine_y = { 'location' },
+        lualine_z = { current_time }
+    }
 })
