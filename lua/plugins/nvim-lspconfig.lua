0b134f5cdc62fedb199d99a1808b45104dfbf107
kallebysantos <kalleby_santos@hotmail.com>
2026-01-19 16:53:31 +0000

    stamp: commenting ts config

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -3,18 +3,18 @@ return {
   opts = {
     servers = {
       -- Typescript
-      denols = {
-        root_dir = require("lspconfig").util.root_pattern("deno.json", "deno.jsonc"),
-      },
-      tsserver = {
-        root_dir = require("lspconfig").util.root_pattern("package.json"),
-        single_file_support = false,
-      },
-      vtsls = {
-        enabled = true,
-        root_dir = require("lspconfig").util.root_pattern("package.json"),
-        single_file_support = false,
-      },
+      -- denols = {
+      --   root_dir = require("lspconfig").util.root_pattern("deno.json", "deno.jsonc"),
+      -- },
+      -- tsserver = {
+      --   root_dir = require("lspconfig").util.root_pattern("package.json"),
+      --   single_file_support = false,
+      -- },
+      -- vtsls = {
+      --   enabled = true,
+      --   root_dir = require("lspconfig").util.root_pattern("package.json"),
+      --   single_file_support = false,
+      -- },
       -- PostgreSQL
       postgres_lsp = {
         enabled = false,
